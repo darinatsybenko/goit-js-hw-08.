e477fbbf00a0fdfe524083cb24f92b44af8d9f8d
darinatsybenko <110915599+darinatsybenko@users.noreply.github.com>
2022-12-25 17:44:06 +0300

    js
    add library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
+console.log(SimpleLightbox);
